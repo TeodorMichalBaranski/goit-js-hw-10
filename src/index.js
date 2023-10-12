8816a8e5f107a7dd017362c9d2cd3311976b29d6
TeodorMichalBaranski <126625089+TeodorMichalBaranski@users.noreply.github.com>
2023-10-12 18:55:21 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,86 +1,85 @@
-import SlimSelect from 'slim-select';                            //import narzędzi
+import SlimSelect from 'slim-select'; //import narzędzi
 import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const catInfo = document.querySelector('.cat-info');         // wskazanie miejsca na stronie gdzie będą wyświetlane informacje o kotach
-let select;                                                             // zmienna do obsługi listy
+const catInfo = document.querySelector('.cat-info'); // wskazanie miejsca na stronie gdzie będą wyświetlane informacje o kotach
+let select; // zmienna do obsługi listy
 
 Notiflix.Loading.standard('Loading...', {
-   // wyświetlenie komunikatu "loadnig"
-    backgroundColor: 'rgba(0,0,0,0.8)',
-});  
-
-window.addEventListener('DOMContentLoaded', () => {
-                                                                                                   // ładowanie całej strony
-Notiflix.Loading.standard('Loading data, please wait...', {
-                                                                                                    // komunikat o ładowaniu strony
-    backgroundColor: 'rgba(0,0,0,0.8)',
+  // wyświetlenie komunikatu "loadnig"
+  backgroundColor: 'rgba(0,0,0,0.8)',
 });
 
-select = new SlimSelect({
+window.addEventListener('DOMContentLoaded', () => {
+  // ładowanie całej strony
+  Notiflix.Loading.standard('Loading data, please wait...', {
+    // komunikat o ładowaniu strony
+    backgroundColor: 'rgba(0,0,0,0.8)',
+  });
+
+  select = new SlimSelect({
     select: '.breed-select',
     events: {
-        afterChange: data => {
+      afterChange: data => {
         const selectedBreedId = data[0].value;
 
-            if (selectedBreedId) {
-            Notiflix.Loading.standard('Loading data, please wait...', {
+        if (selectedBreedId) {
+          Notiflix.Loading.standard('Loading data, please wait...', {
+            // komunikat o ładowaniu strony
             backgroundColor: 'rgba(0,0,0,0.8)',
-            });
-            fetchCatByBreed(selectedBreedId)
+          });
+          fetchCatByBreed(selectedBreedId)
             .then(catData => {
-                console.log(catData);
-                updateCatInfo(catData);
-                Notiflix.Loading.remove();
+              console.log(catData);
+              CatInfo(catData);
+              Notiflix.Loading.remove();
             })
             .catch(error => {
-                Notiflix.Loading.remove();
-                Notiflix.Notify.failure(
+              Notiflix.Loading.remove();
+              Notiflix.Notify.failure(
                 'Oops! Something goes wrong! Try to reload this page!'
-                );
+              );
             });
         }
-        },
+      },
     },
-    });
+  });
 
   fetchBreeds() // pobranie danych o rasach
     .then(breeds => {
       Notiflix.Loading.remove(); // usunięcie komunikatu "loading"
-        select.setData(
+      select.setData(
         // wypełnienie listy rozwijanej danymi o rasach
         breeds.map(breed => ({
-            text: breed.name,
-            value: breed.id,
-            }))
-        );
+          text: breed.name,
+          value: breed.id,
+        }))
+      );
     })
     .catch(error => {
-        Notiflix.Loading.remove(); // usunięcie napisu "loading" i wyświetlenie komunikatu o błędzie
-        Notiflix.Notify.failure(
+      Notiflix.Loading.remove();
+      Notiflix.Notify.failure(
         'Oops! Something went wrong! Try reloading the page!'
-        );
-        
+      );
     });
 
-function updateCatInfo(catData) {
-    // funkcja aktualizująca info o kocie na stronie
+  function CatInfo(catData) {
     const { url, breeds } = catData;
 
     if (breeds.length > 0) {
-    const { name, description, temperament } = breeds[0];
+      const { name, description, temperament } = breeds[0];
 
-    catInfo.innerHTML = `
-    <img src="${url}" alt="${breeds.name}" />
+      catInfo.innerHTML = `
+    <img src="${url}" alt="${breeds.name}" style="width:500px;height:500px;"/>
     <div class="description">
     <h2>${name}</h2>
     <p>${description}</p>
     <p>${temperament}</p>
     </div>
     `;
-        }
     }
+  }
 });
